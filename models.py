0179e74dcc2cf922431f3c4ccb39b823a9f57e2c
Austin Stewart <aaaustin10@gmail.com>
2014-06-21 21:02:39 -0700

    Adds some model imformation

--- models.py
+++ models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Group(models.Model):
+  users = models.ManyToManyField(User)
+  group_name = models.CharField(max_length=30)
+
+class List(models.Model):
+  owner = models.ForeignKey(User)
+  name = models.CharField(max_length=30)
+  groups = models.ManyToManyField(Group)
+
+class Item(models.Model):
+  quantity = models.FloatField()
+  unit_cost = models.FloatField()
+  name = models.CharField(max_length=30)
+  units = models.IntegerField()
+  lyst = models.ForeignKey(List)
+
