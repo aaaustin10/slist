cf4ed5d9e16ce93b7f159d26a10e2d1779aa88ca
Austin Stewart <aaaustin10@gmail.com>
2014-06-22 20:57:38 -0700

    Gives better names to database items and adds initial migration information

--- models.py
+++ models.py
@@ -3,17 +3,17 @@ from django.contrib.auth.models import User
 
 class Group(models.Model):
   users = models.ManyToManyField(User)
-  group_name = models.CharField(max_length=30)
+  name = models.CharField(max_length=30)
 
 class List(models.Model):
   owner = models.ForeignKey(User)
   name = models.CharField(max_length=30)
-  groups = models.ManyToManyField(Group)
+  group = models.ManyToManyField(Group)
 
 class Item(models.Model):
   quantity = models.FloatField()
   unit_cost = models.FloatField()
   name = models.CharField(max_length=30)
   units = models.IntegerField()
-  lyst = models.ForeignKey(List)
+  parent = models.ForeignKey(List)
 
