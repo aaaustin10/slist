81957af07f470338cf140a63cd36aae025251093
Austin Stewart <aaaustin10@gmail.com>
2014-08-07 21:06:10 -0700

    Adds the beginning of the lists page.

--- urls.py
+++ urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from slist import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index.as_view(), name='index')
+    url(r'^$', views.index.as_view(), name='index'),
+    url(r'^lists$', views.lists.as_view(), name='lists')
   )
--- views.py
+++ views.py
@@ -1,4 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 class index(TemplateView):
   template_name = "index.html"
+
+class lists(ListView):
+  context_object_name = "lists"
+  template_name = "lists.html"
+  def get_queryset(self):
+    return []
